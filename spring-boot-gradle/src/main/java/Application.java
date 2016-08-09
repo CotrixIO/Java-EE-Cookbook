d8bbce5305e85cf80013bc51e533eedd6ec1b32d
Stijn De Mulder <stijn.demulder@elqoo.com>
2016-08-09 22:57:29 +0200

    Initial setup livereload

--- spring-boot-gradle/src/main/java/Application.java
+++ spring-boot-gradle/src/main/java/Application.java
@@ -1,8 +1,6 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -10,15 +8,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication(exclude = {JacksonAutoConfiguration.class})
 @EnableTransactionManagement
 @EnableScheduling
-@EnableEurekaClient
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    public AlwaysSampler defaultSampler() {
-        return new AlwaysSampler();
-    }
 }
