6e7c8432b1c581d36bb16e98764a8f10c8296077
Stijn De Mulder <stijn.demulder@elqoo.com>
2016-08-18 08:38:42 +0200

    Add spring boot project setup

--- spring-boot-gradle/src/main/java/io/cotrix/jeecookbook/springboot/Application.java
+++ spring-boot-gradle/src/main/java/io/cotrix/jeecookbook/springboot/Application.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication(exclude = {JacksonAutoConfiguration.class})
-@EnableTransactionManagement
-@EnableScheduling
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
